fb495c2960f7fd8e73df2ae8b63109fe10857a20
seyed <smbsh20@gmail.com>
2022-06-24 02:01:15 +0430

    add template and first view

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR) +'/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#on developing
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, "assets")]
 
+#on server
+# STATIC_ROOT='../public_html/site.ir/static'
+# MEDIA_ROOT='../public_html/site.ir/media'
+
+# for developing in device
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -1,7 +1,6 @@
-"""mysite URL Configuration
-
+"""smart farm URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +12,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import handler404, handler500
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from .views import panel
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',panel),
+    # path('log/',include('logapp.urls')),
+    # path('account/',include('account_app.urls')),
+    # path('header', header, name="header"),
+    # path('footer', footer, name="footer"),
+    # path('email/',include('email_app.urls')),
+    # path('pass/',include('password_app.urls')),
 ]

7238731fcc6f8f5d551fb7db2b41acc97deeeaf9
seyed <smbsh20@gmail.com>
2022-06-28 15:40:18 +0430

    fix bug in model

--- src/decription_app/admin.py
+++ src/decription_app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import MyData
 # Register your models here.
 class SensorData(admin.ModelAdmin):
-    list_display = ['user','date_added']
-    list_filter = ['user',  'date_added']
+    list_display = ['user','temp','date_added']
+    list_filter = ['user','temp','date_added']
     # list_editable = ['code']
-    search_fields = ['user',  'date_added']
+    search_fields = ['user','temp','date_added']
 
 admin.site.register(MyData,SensorData)

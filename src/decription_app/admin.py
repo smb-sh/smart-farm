3603526f2fd566bac7dab1dd921cae8e24f8b641
seyed <smbsh20@gmail.com>
2022-06-28 15:43:24 +0430

    admin panel changed

--- src/decription_app/admin.py
+++ src/decription_app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import MyData
 # Register your models here.
 class SensorData(admin.ModelAdmin):
-    list_display = ['user','temp','date_added']
-    list_filter = ['user','temp','date_added']
+    list_display = ['user','temp','humi','soilm','rays','rain','date_added']
+    list_filter = ['user','temp','humi','soilm','rays','rain','date_added']
     # list_editable = ['code']
-    search_fields = ['user','temp','date_added']
+    search_fields = ['user','temp','humi','soilm','rays','rain','date_added']
 
 admin.site.register(MyData,SensorData)
